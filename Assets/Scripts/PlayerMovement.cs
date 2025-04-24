ff20ff27684a85f002d19dbd9e6c820a7de6e645
Supitcha5479 <supitcha.prat@bumail.net>
2025-04-24 20:53:51 +0700

    jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,48 @@
 using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Rigidbody2D rb2d;
+    //walk 
+    float move;
+    [SerializeField] float speed;
+    //jump
+    [SerializeField] float jumpForce;
+    [SerializeField] bool isjumping;
+    
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        move = Input.GetAxis("Horizontal"); //เป็นชุดคีย์ ไปดูในอีดิทโปรเจคเซ็ตติ้ง
+
+        rb2d.linearVelocity = new Vector2(move * speed, rb2d.linearVelocity.y);
+
+        if (Input.GetButtonDown("Jump") && !isjumping)
+        {
+            rb2d.AddForce(new Vector2(rb2d.linearVelocity.x, jumpForce));
+            Debug.Log("Jump!"); //โดดได้จริง
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isjumping = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isjumping = true;
+        }
     }
 }
